e9c5e5780362eb7478924e70036618e21946ff8c
jimatlang <jim.stewart@thelangschool.org>
2024-09-10 13:06:03 -0400

    created stub for User account

---
+++ UserAccount/src/com/lang/useraccount/App.java
@@ -0,0 +1,10 @@
+package com.lang.useraccount;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        new App();
+    }
+    public App(){
+        User user1 = new User("whataboutbob", 5673, "Robert", "Smith", "bob@company.com");
+    }
+}
---
+++ UserAccount/src/com/lang/useraccount/User.java
@@ -0,0 +1,9 @@
+package com.lang.useraccount;
+
+public class User {
+    // TODO: create userName, id, firstname, lastname and email properties
+    public User(String userName, int id, String firstName, String lastName, String email){
+        //TODO: set the properties in the constructor
+    }
+    // TODO: create all the getters and setters
+}
